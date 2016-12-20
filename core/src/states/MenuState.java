c3f6fe0a4f804a0f2c65a311262b8ac4c43de9ee
munta <munta@DESKTOP-JLPPGJB>
2016-12-20 14:23:22 -0500

    backrounds loaded in

--- core/src/states/MenuState.java
+++ core/src/states/MenuState.java
@@ -14,44 +14,43 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  * @author munta
  */
 public class MenuState extends State {
+
     //the backround
+
     private Texture bg;
-    //an integer to store highscore
-    private Texture button;
+
     private int highScore;
     //the font
     private BitmapFont font;
-    
-    public MenuState (StateManager sm){
+
+    public MenuState(StateManager sm) {
+        super(sm);
         //add the backround texture image
         bg = new Texture("Backround.jpg");
-        //add the the playbutton texture image
-        button = new Texture("playbutton.png");
-        
-        
+
     }
 
     @Override
     public void render(SpriteBatch batch) {
-      batch.begin();
-      batch.draw(bg, 0, 0, getViewWidth(), getViewHeight());
+        batch.begin();
+        batch.draw(bg, 0, 0, getViewWidth(), getViewHeight());
+        batch.end();
     }
 
     @Override
     public void update(float deltaTime) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
     public void handleInput() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
     @Override
     public void dispose() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        bg.dispose();
+
     }
-    
-    
-    
+
 }
