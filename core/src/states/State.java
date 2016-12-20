07780f4265048f2b8369f535b5089d4d8f4828af
joudn2217 <joudn2217@HRH-IC0027338.wrdsb.ca>
2016-12-20 13:51:55 -0500

    states

--- core/src/states/State.java
+++ core/src/states/State.java
@@ -4,10 +4,33 @@
  */
 package states;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  *
  * @author joudn2217
  */
-public class State {
+public abstract class State {
+    private OrthographicCamera cam;
+    private StateManager stateManager;
+    
+    public State(StateManager sm){
+        cam = new OrthographicCamera();
+        stateManager = sm;
+    }
+    
+    public abstract void render(SpriteBatch batch);
+    public abstract void update(float deltaTime);
+    public abstract void handleInput();
+    public abstract void dispose();
+    
+    public StateManager getStateManager(){
+        return stateManager;
+    }
+    
+    public OrthographicCamera getCamera(){
+        return cam;
+    }
     
 }
