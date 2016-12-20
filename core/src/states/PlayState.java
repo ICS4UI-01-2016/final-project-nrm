9da46374ad0eb3a2584e55df27b26d09f3318915
voigr4865 <voigr4865@HRH-IC0027337.wrdsb.ca>
2016-12-20 14:24:34 -0500

    State

--- core/src/states/PlayState.java
+++ core/src/states/PlayState.java
@@ -4,10 +4,55 @@
  */
 package states;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.MyGdxGame;
+
 /**
  *
  * @author voigr4865
  */
-public class PlayState {
+public class PlayState extends State{
+    
+    private SpriteBatch batch;
+    private Texture bg;
+    
+    public PlayState(StateManager sm){
+        super(sm);
+        
+        setCameraView(MyGdxGame.WIDTH / 2, MyGdxGame.HEIGHT / 2);
+        
+        bg = new Texture("bg.jpg");
+  
+        
+        
+    }
+    
+    @Override
+    public void render(SpriteBatch batch){
+        
+        batch.setProjectionMatrix(getCombinedCamera());
+        
+        batch.begin();
+        
+        batch.draw(bg, getCameraX() - getViewWidth() / 2, getCameraY() - getViewHeight() / 2);
+        
+        batch.end();
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        
+    }
+
+    @Override
+    public void handleInput() {
+        
+    }
+
+    @Override
+    public void dispose() {
+        
+    }
     
 }
