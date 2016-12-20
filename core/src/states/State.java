575ba6da8db82c663e99e3e2e40016eb22f02ad1
joudn2217 <joudn2217@HRH-IC0027338.wrdsb.ca>
2016-12-20 14:03:54 -0500

    done

--- core/src/states/State.java
+++ core/src/states/State.java
@@ -6,6 +6,7 @@ package states;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
 
 /**
  *
@@ -31,6 +32,31 @@ public abstract class State {
     
     public OrthographicCamera getCamera(){
         return cam;
+    }
+    
+    public void setCameraView(float width, float height){
+        cam.setToOrtho(false, width, height);
+        cam.update();
+    }
+    
+    public Matrix4 getCombinedCamera(){
+        return cam.combined;
+    }
+    
+    public float getCameraX(){
+        return cam.position.x;
+    }
+    
+    public float getCameraY(){
+        return cam.position.y;
+    }
+    
+    public float getViewWidth(){
+        return cam.viewportWidth;
+    }
+    
+    public float getViewHeight(){
+        return cam.viewportHeight;
     }
     
 }
