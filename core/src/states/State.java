3dd8b33d8ea97ee4bbc64bd2191c46011799a531
joudn2217 <joudn2217@HRH-IC0027338.wrdsb.ca>
2016-12-20 13:37:52 -0500

    states

---
+++ core/src/states/State.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package states;
+
+/**
+ *
+ * @author joudn2217
+ */
+public class State {
+    
+}
